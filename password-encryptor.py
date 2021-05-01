6caa09f26a8b67cc15b8f1625700ebf4d8ec71c0
Roy Romarate <royromarate@gmail.com>
2021-05-01 15:12:42 +0800

    added pad function

--- password-encryptor.py
+++ password-encryptor.py
@@ -8,19 +8,27 @@ from Crypto.Cipher import AES
 class PasswordEncryptor():
     """Encrypts or decrypts a given value."""
     # TODO: validate inputs
-    # TODO: pad value if necessary and include size / size of padding added
     # TODO: error handling
+
+    _BLOCK_SIZE = 16
+
+    @staticmethod
+    def _pad_value(value):
+        """Pads value to reach a length divisible by the requried block size"""
+        block_size = PasswordEncryptor._BLOCK_SIZE
+        return str(value) + " " * (block_size - len(value) % PasswordEncryptor._BLOCK_SIZE)
 
     @staticmethod
     def decrypt(encrypted_value, key, initialization_vector):
         """Returns the decrypted value using the provided key and initialization vector"""
         encryptor = AES.new(key, AES.MODE_CBC, initialization_vector)
-        return encryptor.decrypt(b64decode(encrypted_value)).decode("utf-8")
+        return encryptor.decrypt(b64decode(encrypted_value)).decode("utf-8").rstrip()
 
     @staticmethod
     def encrypt(value, key, initialization_vector):
         """Returns the encrypted value using provided the key and initialization vector"""
         encryptor = AES.new(key, AES.MODE_CBC, initialization_vector)
+        value = PasswordEncryptor._pad_value(value)
         return b64encode(encryptor.encrypt(value)).decode("utf-8")
 
 
@@ -64,7 +72,6 @@ class PasswordEncryptorApp():
 
     def run(self):
         args = self.args
-        # print(len(args.value), len(args.key), len(args.iv))
         if args.decrypt_mode:
             sys.stdout.write(PasswordEncryptor().decrypt(args.value, args.key, args.iv) + "\n")
             return 0
